99e186fc6bb20895529eb113ed45f79816dd97d9
Xulio Xesús León Pérez <xulioxesus@gmail.com>
2024-12-09 17:21:16 +0100

    Alternativa con NavMeshes

--- Assets/Scripts/FollowWaypoints.cs
+++ Assets/Scripts/FollowWaypoints.cs
@@ -1,80 +1,95 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class FollowWaypoints : MonoBehaviour {
 
-    Transform goal;
-    float speed = 5.0f;
-    float accuracy = 5.0f;
-    float rotSpeed = 2.0f;
+    // Transform goal;
+    // float speed = 5.0f;
+    // float accuracy = 5.0f;
+    // float rotSpeed = 2.0f;
+
     GameObject[] wps;
     GameObject currentNode;
-    int currentWP = 0;
-    Graph g;
+    NavMeshAgent agent;
+
+    // int currentWP = 0;
+    // Graph g;
 
     public GameObject wpManager;
 
     void Start() {
         Time.timeScale = 5.0f;
         wps = wpManager.GetComponent<WPManager>().waypoints;
-        g = wpManager.GetComponent<WPManager>().graph;
+        // g = wpManager.GetComponent<WPManager>().graph;
         currentNode = wps[0];
+
+        agent = GetComponent<NavMeshAgent>();
 
         // Invoke("GotoRuin", 2.0f);
     }
 
     public void GotoHeli() {
 
-        g.AStar(currentNode, wps[0]);
-        currentWP = 0;
+        //g.AStar(currentNode, wps[0]);
+        //currentWP = 0;
+        GotoHere(0);
     }
 
     public void GotoRuin() {
 
-        g.AStar(currentNode, wps[7]);
-        currentWP = 0;
+        //g.AStar(currentNode, wps[7]);
+        //currentWP = 0;
+        GotoHere(7);
     }
 
     public void GotoRock() {
 
-        g.AStar(currentNode, wps[1]);
-        currentWP = 0;
+        //g.AStar(currentNode, wps[1]);
+        //currentWP = 0;
+        GotoHere(1);
     }
 
     public void GotoFactory() {
 
-        g.AStar(currentNode, wps[4]);
-        currentWP = 0;
+        //g.AStar(currentNode, wps[4]);
+        //currentWP = 0;
+        GotoHere(4);
+    }
+
+    void GotoHere(int value) {
+
+        agent.SetDestination(wps[value].transform.position);
     }
 
     void LateUpdate() {
 
-        if (g.pathList.Count == 0 || currentWP == g.pathList.Count) return;
+        //if (g.pathList.Count == 0 || currentWP == g.pathList.Count) return;
 
-        currentNode = g.getPathPoint(currentWP);
+        //currentNode = g.getPathPoint(currentWP);
 
-        if (Vector3.Distance(g.pathList[currentWP].getID().transform.position, transform.position) < accuracy) {
+        //if (Vector3.Distance(g.pathList[currentWP].getID().transform.position, transform.position) < accuracy) {
 
-            currentWP++;
-        }
+        //    currentWP++;
+        //}
 
-        if (currentWP < g.pathList.Count) {
+        //if (currentWP < g.pathList.Count) {
 
-            goal = g.pathList[currentWP].getID().transform;
-            Vector3 lookAtGoal = new Vector3(
-                goal.position.x,
-                transform.position.y,
-                goal.position.z);
+        //    goal = g.pathList[currentWP].getID().transform;
+        //    Vector3 lookAtGoal = new Vector3(
+        //        goal.position.x,
+        //        transform.position.y,
+        //        goal.position.z);
 
-            Vector3 direction = lookAtGoal - this.transform.position;
+        //    Vector3 direction = lookAtGoal - this.transform.position;
 
-            transform.rotation = Quaternion.Slerp(
-                this.transform.rotation,
-                Quaternion.LookRotation(direction),
-                Time.deltaTime * rotSpeed);
+        //    transform.rotation = Quaternion.Slerp(
+        //        this.transform.rotation,
+        //        Quaternion.LookRotation(direction),
+        //        Time.deltaTime * rotSpeed);
 
-            transform.Translate(0.0f, 0.0f, speed * Time.deltaTime);
-        }
+        //    transform.Translate(0.0f, 0.0f, speed * Time.deltaTime);
+        //}
     }
 }
